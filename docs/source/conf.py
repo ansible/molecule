49bb1152bb8fefa4257bf3fc58fa67b97ee4d7d7
Adam Brown <adambr2@cisco.com>
2015-11-25 12:44:39 -0600

    docs fix for RTD support

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -115,7 +115,13 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 # html_theme = 'alabaster'
-html_theme = 'bootstrap'
+
+# need to use default theme for readthedocs
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if on_rtd:
+    html_theme = 'default'
+else:
+    html_theme = 'bootstrap'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
