d0f486e255d27e24c2a77e038d9ac53bfe9d125c
John Dewey <john@dewey.ws>
2016-06-29 19:00:04 -0700

    Would never update RHEL based images
    Required a `-y` flag to `yum update`.  Also, added `yum makecache` prior
    to upgrade.

--- molecule/provisioners.py
+++ molecule/provisioners.py
@@ -476,8 +476,8 @@ class DockerProvisioner(BaseProvisioner):
 
             dockerfile = '''
             FROM {}:{}
-            RUN bash -c 'if [ -x "$(command -v apt-get)" ]; then  apt-get update && apt-get install -y python sudo; fi'
-            RUN bash -c 'if [ -x "$(command -v yum)" ]; then  yum update && yum install -y python sudo; fi'
+            RUN bash -c 'if [ -x "$(command -v apt-get)" ]; then apt-get update && apt-get install -y python sudo; fi'
+            RUN bash -c 'if [ -x "$(command -v yum)" ]; then yum makecache fast && yum update -y && yum install -y python sudo; fi'
 
             '''
 
