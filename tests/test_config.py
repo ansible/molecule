ansible/molecule

commit 4261d0c7f979c6a5a24d099b0ae3c7a26787e42f

Implemented a new config class (#268)
Integrated a config class that doesn't involve having to maintain
custom dictionary merging code.  `Anyconfig`[0] is a great tool, with
far more sweat and tears invested in it.

* Molecule config is hard coded to simply `molecule.yml` in the current
  directory.  It no longer allows various locations to find a config.
* The config loading is default -> project -> local.  This means a local
  config can override anything else.
* Implemented tests which utilize a tmpdir config fixture.
* Updated documentation

[0] https://github.com/ssato/python-anyconfig

Fixes: #260

molecule/commands/init.py
@@ -98,9 +98,7 @@ class Init(base.BaseCommand):
 
         sanitized_role = re.sub('[._]', '-', role)
         with open(
-                os.path.join(
-                    role_path,
-                    self.molecule._config.config['molecule']['molecule_file']),
+                os.path.join(role_path, self.molecule._config.molecule_file),
                 'w') as f:
             f.write(t_molecule.render(config=self.molecule._config.config,
                                       role=sanitized_role))

molecule/config.py
@@ -19,80 +19,24 @@
 #  THE SOFTWARE.
 
 import os
-import sys
+import os.path
 
-import colorama
-import yaml
+import anyconfig
 
 from molecule import utilities
 
+CONFIG_PATHS = [os.path.join(
+    os.path.dirname(__file__), 'conf/defaults.yml'), 'molecule.yml',
+                '~/.config/molecule/config.yml']
+
 
 class Config(object):
-    # locations to look for molecule config files
-    CONFIG_PATHS = [os.environ.get('MOLECULE_CONFIG'),
-                    os.path.expanduser('~/.config/molecule/config.yml'),
-                    '/etc/molecule/config.yml']
+    def __init__(self, configs=CONFIG_PATHS):
+        self.config = self._get_config(configs)
 
-    def load_defaults_file(self, defaults_file=None):
-        """
-        Loads config from a file
-
-        :param defaults_file: optional YAML file to open and read defaults from
-        :return: None
-        """
-        # load defaults from provided file
-        if defaults_file is None:
-            defaults_file = os.path.join(
-                os.path.dirname(__file__), 'conf/defaults.yml')
-
-        with open(defaults_file, 'r') as stream:
-            self.config = yaml.safe_load(stream)
-
-    def merge_molecule_config_files(self, paths=CONFIG_PATHS):
-        """
-        Looks for a molecule config file in paths and merges it with current config if found
-
-        Only the first file that's found will be merged in.
-        :param paths: list of places to look for config files
-        :return: Path of file that was merged into config, if found, otherwise None
-        """
-        # merge defaults with a config file if found
-        for path in paths:
-            if path and os.path.isfile(path):
-                with open(path, 'r') as stream:
-                    self.config = utilities.merge_dicts(self.config,
-                                                        yaml.safe_load(stream))
-                    return path
-        return
-
-    def merge_molecule_file(self, molecule_file=None):
-        """
-        Looks for a molecule file in the local path and merges it into our config
-
-        :param molecule_file: path and name of molecule file to look for
-        :return: None
-        """
-        if molecule_file is None:
-            molecule_file = self.config['molecule']['molecule_file']
-
-        if not os.path.isfile(molecule_file):
-            error = '\n{}Unable to find {}. Exiting.{}'
-            utilities.logger.error(error.format(colorama.Fore.RED, self.config[
-                'molecule']['molecule_file'], colorama.Fore.RESET))
-            sys.exit(1)
-
-        with open(molecule_file, 'r') as env:
-
-            try:
-                molecule_yml = yaml.load(env)
-            except Exception as e:
-                error = "\n{}{} isn't properly formatted: {}{}"
-                utilities.logger.error(error.format(
-                    colorama.Fore.RED, molecule_file, e, colorama.Fore.RESET))
-                sys.exit(1)
-
-            interim = utilities.merge_dicts(self.config, molecule_yml)
-            self.config = interim
+    @property
+    def molecule_file(self):
+        return 'molecule.yml'
 
     def build_easy_paths(self):
         """
@@ -143,3 +87,30 @@ class Config(object):
             instance['vm_name'] = utilities.format_instance_name(
                 instance['name'], platform,
                 self.config['vagrant']['instances'])
+
+    def _get_config(self, configs):
+        if not self._has_molecule_file():
+            error = '\nUnable to find {}. Exiting.'
+            utilities.logger.error(error.format(self.molecule_file))
+            utilities.sysexit()
+
+        return self._combine(configs)
+
+    def _has_molecule_file(self):
+        return os.path.isfile(self.molecule_file)
+
+    def _combine(self, configs):
+        """ Perform a prioritized recursive merge of serveral source files,
+        and return a new dict.
+
+        The merge order is based on the index of the list, meaning that elements
+        at the end of the list will be merged last, and have greater precedence
+        than elements at the beginning.
+
+        :param configs: A list containing the yaml files to load.
+        :return: dict
+        """
+
+        return anyconfig.load(configs,
+                              ignore_missing=True,
+                              ac_merge=anyconfig.MS_DICTS_AND_LISTS)

molecule/core.py
@@ -48,18 +48,8 @@ class Molecule(object):
         self._provisioner = None
 
     def main(self):
-        # load molecule defaults
-        self._config.load_defaults_file()
-
-        # merge in any molecule config files found (eg: ~/.configs/molecule/config.yml)
-        self._config.merge_molecule_config_files()
-
-        # init command doesn't need to load molecule.yml
         if self._args.get('<command>') == 'init':
             return  # exits program
-
-        # merge in molecule.yml
-        self._config.merge_molecule_file()
 
         # ensure the .molecule directory exists
         if not os.path.isdir(os.path.join(os.curdir, self._config.config[
@@ -85,7 +75,7 @@ class Molecule(object):
             self._args['--platform'] = None
             self._provisioner = self.get_provisioner()
             self._print_valid_providers()
-            sys.exit(1)
+            utilities.sysexit()
         except baseprovisioner.InvalidPlatformSpecified:
             utilities.logger.error("\nInvalid platform '{}'\n".format(
                 self._args['--platform']))
@@ -93,7 +83,7 @@ class Molecule(object):
             self._args['--platform'] = None
             self._provisioner = self.get_provisioner()
             self._print_valid_platforms()
-            sys.exit(1)
+            utilities.sysexit()
 
         if not os.path.exists(self._config.config['molecule']['molecule_dir']):
             os.makedirs(self._config.config['molecule']['molecule_dir'])
@@ -128,7 +118,7 @@ class Molecule(object):
         except subprocess.CalledProcessError as e:
             utilities.logger.error('ERROR: {}'.format(e))
             utilities.logger.error("Does your vagrant VM exist?")
-            sys.exit(e.returncode)
+            utilities.sysexit(e.returncode)
         utilities.write_file(ssh_config, out)
 
     def _print_valid_platforms(self, porcelain=False):
@@ -220,7 +210,7 @@ class Molecule(object):
 
         # rakefile
         kwargs = {
-            'molecule_file': self._config.config['molecule']['molecule_file'],
+            'molecule_file': self._config.molecule_file,
             'current_platform': self._env['MOLECULE_PLATFORM'],
             'serverspec_dir': self._config.config['molecule']['serverspec_dir']
         }
@@ -317,7 +307,7 @@ class Molecule(object):
                 'ERROR: the group_vars path {} does not exist. Check your configuration file'.format(
                     group_vars_target))
 
-            sys.exit(1)
+            utilities.sysexit()
         os.symlink(group_vars_target, group_vars_link_path)
 
     def _display_tabulate_data(self, data, headers=None):

tests/conftest.py
@@ -17,3 +17,39 @@
 #  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 #  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 #  THE SOFTWARE.
+
+import os
+import os.path
+
+import pytest
+
+
+@pytest.fixture()
+def temp_files(tmpdir, request):
+    def wrapper(content=[]):
+        d = tmpdir.mkdir('molecule')
+        confs = []
+        for index, item in enumerate(content):
+            c = d.join(os.extsep.join((str(index), 'yml')))
+            c.write(item)
+            confs.append(c.strpath)
+
+        def cleanup():
+            for c in confs:
+                os.remove(c)
+            os.rmdir(d.strpath)
+
+        request.addfinalizer(cleanup)
+
+        return confs
+
+    return wrapper
+
+
+@pytest.fixture()
+def mock_molecule_file_exists(monkeypatch):
+    def mockreturn(m):
+        return True
+
+    return monkeypatch.setattr('molecule.config.Config._has_molecule_file',
+                               mockreturn)

tests/provisioner/test_dockerprovisioner.py
@@ -18,10 +18,9 @@
 #  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 #  THE SOFTWARE.
 
-import os
-
 import pytest
 
+from molecule import config
 from molecule import core
 from molecule import ansible_playbook
 from molecule.provisioners import dockerprovisioner
@@ -30,10 +29,8 @@ from molecule.provisioners import dockerprovisioner
 
 
 @pytest.fixture()
-def molecule_file(tmpdir, request):
-    d = tmpdir.mkdir('molecule')
-    c = d.join(os.extsep.join(('molecule', 'yml')))
-    data = {
+def docker_data():
+    return {
         'molecule': {
             'molecule_dir': '.test_molecule',
             'inventory_file': 'tests/support/ansible_inventory'
@@ -49,98 +46,80 @@ def molecule_file(tmpdir, request):
                                                  'ansible_groups':
                                                  ['group2']}
             ]
-        },
-        'ansible': {
-            'config_file': 'test_config',
-            'inventory_file': 'test_inventory'
         }
     }
-    c.write(data)
-
-    def cleanup():
-        os.remove(c.strpath)
-        os.rmdir(d.strpath)
-
-    request.addfinalizer(cleanup)
-
-    return c.strpath
 
 
 @pytest.fixture()
-def molecule(molecule_file):
+def molecule_instance(temp_files, docker_data, mock_molecule_file_exists):
+    c = temp_files(content=[docker_data])
     m = core.Molecule(dict())
-    m._config.load_defaults_file(defaults_file=molecule_file)
+    m._config = config.Config(configs=c)
 
     return m
 
 
-def test_name(molecule):
-    docker_provisioner = dockerprovisioner.DockerProvisioner(molecule)
+@pytest.fixture()
+def docker_instance(molecule_instance):
+    d = dockerprovisioner.DockerProvisioner(molecule_instance)
 
+    return d
+
+
+def test_name(docker_instance):
     # false values don't exist in arg dict at all
-    assert 'docker' == docker_provisioner.name
+    assert 'docker' == docker_instance.name
 
 
-def test_get_provisioner(molecule):
-    assert 'docker' == molecule.get_provisioner().name
+def test_get_provisioner(molecule_instance):
+    assert 'docker' == molecule_instance.get_provisioner().name
 
 
-def test_up(molecule):
-    docker_provisioner = dockerprovisioner.DockerProvisioner(molecule)
-    docker_provisioner.up()
-    docker_provisioner.destroy()
+def test_up(docker_instance):
+    docker_instance.up()
+    docker_instance.destroy()
 
 
-def test_instances(molecule):
-    docker_provisioner = dockerprovisioner.DockerProvisioner(molecule)
+def test_instances(docker_instance):
+    assert 'test1' == docker_instance.instances[0]['name']
+    assert 'test2' == docker_instance.instances[1]['name']
 
-    assert 'test1' == docker_provisioner.instances[0]['name']
-    assert 'test2' == docker_provisioner.instances[1]['name']
-
-    docker_provisioner.destroy()
+    docker_instance.destroy()
 
 
-def test_status(molecule):
-    docker_provisioner = dockerprovisioner.DockerProvisioner(molecule)
+def test_status(docker_instance):
+    docker_instance.up()
 
-    docker_provisioner.up()
+    assert 'test1' == docker_instance.status()[1].name
+    assert 'test2' == docker_instance.status()[0].name
 
-    assert 'test1' == docker_provisioner.status()[1].name
-    assert 'test2' == docker_provisioner.status()[0].name
+    assert 'Up' in docker_instance.status()[1].state
+    assert 'Up' in docker_instance.status()[0].state
 
-    assert 'Up' in docker_provisioner.status()[1].state
-    assert 'Up' in docker_provisioner.status()[0].state
+    assert 'docker' in docker_instance.status()[0].provider
+    assert 'docker' in docker_instance.status()[1].provider
 
-    assert 'docker' in docker_provisioner.status()[0].provider
-    assert 'docker' in docker_provisioner.status()[1].provider
-
-    docker_provisioner.destroy()
+    docker_instance.destroy()
 
 
-def test_destroy(molecule):
-    docker_provisioner = dockerprovisioner.DockerProvisioner(molecule)
+def test_destroy(docker_instance):
+    docker_instance.up()
 
-    docker_provisioner.up()
+    assert 'test1' == docker_instance.status()[1].name
+    assert 'test2' == docker_instance.status()[0].name
 
-    assert 'test1' == docker_provisioner.status()[1].name
-    assert 'test2' == docker_provisioner.status()[0].name
+    assert 'Up' in docker_instance.status()[1].state
+    assert 'Up' in docker_instance.status()[0].state
 
-    assert 'Up' in docker_provisioner.status()[1].state
-    assert 'Up' in docker_provisioner.status()[0].state
+    docker_instance.destroy()
 
-    docker_provisioner.destroy()
-
-    assert 'Not Created' in docker_provisioner.status()[1].state
-    assert 'Not Created' in docker_provisioner.status()[0].state
+    assert 'Not Created' in docker_instance.status()[1].state
+    assert 'Not Created' in docker_instance.status()[0].state
 
 
-def test_provision(molecule):
-    docker_provisioner = dockerprovisioner.DockerProvisioner(molecule)
-
-    docker_provisioner.destroy()
-    docker_provisioner.up()
-
-    pb = docker_provisioner.ansible_connection_params
+def test_provision(docker_instance):
+    docker_instance.up()
+    pb = docker_instance.ansible_connection_params
     pb['playbook'] = 'tests/support/playbook.yml'
     pb['inventory'] = 'test1,test2,'
     ansible = ansible_playbook.AnsiblePlaybook(pb)
@@ -148,16 +127,16 @@ def test_provision(molecule):
     # TODO(retr0h): Understand why provisioner is None
     assert (None, '') == ansible.execute()
 
-    docker_provisioner.destroy()
+    docker_instance.destroy()
 
 
-def test_inventory_generation(molecule):
-    molecule._provisioner = dockerprovisioner.DockerProvisioner(molecule)
+def test_inventory_generation(molecule_instance, docker_instance):
+    molecule_instance._provisioner = docker_instance
 
-    molecule._provisioner.up()
-    molecule._create_inventory_file()
+    molecule_instance._provisioner.up()
+    molecule_instance._create_inventory_file()
 
-    pb = molecule._provisioner.ansible_connection_params
+    pb = molecule_instance._provisioner.ansible_connection_params
     pb['playbook'] = 'tests/support/playbook.yml'
     pb['inventory'] = 'tests/support/ansible_inventory'
     ansible = ansible_playbook.AnsiblePlaybook(pb)
@@ -165,4 +144,4 @@ def test_inventory_generation(molecule):
     assert (None, '') == ansible.execute()
 
     # TODO(retr0h): Understand why provisioner is None
-    molecule._provisioner.destroy()
+    molecule_instance._provisioner.destroy()

commit 0b321fada03c82d82851e94b1e66b9dd84cf4ade

Corrected `molecule init`
Corrected bug introduced in #268.  Removed duplicate "are we running
the init command" logic from both the core and command base class.

The `init` sub command is unique to other commands.  It doesn't require
a `molecule.yml`.  Moved this logic into a method, in the parent class,
so the children can change the behavior.

Also disabled static inventory introduced in #40.  We never used this,
and the core needs refactored before we attempt to reintroduce it.

Fixes: #275

molecule/commands/base.py
@@ -32,58 +32,28 @@ class InvalidHost(Exception):
 class BaseCommand:
     __metaclass__ = abc.ABCMeta
 
-    def __init__(self, command_args, global_args, molecule=False):
+    def __init__(self, command_args, args, molecule=False):
         """
         Initialize commands
 
         :param command_args: arguments of the command
-        :param global_args: arguments from the CLI
+        :param args: arguments from the CLI
         :param molecule: molecule instance
         """
         self.args = docopt.docopt(self.__doc__, argv=command_args)
         self.args['<command>'] = self.__class__.__name__.lower()
         self.command_args = command_args
 
-        self.static = False
+        self.molecule = core.Molecule(self.args)
+        self.main()
 
-        # give us the option to reuse an existing molecule instance
-        if not molecule:
-            self.molecule = core.Molecule(self.args)
-            self.molecule.main()
-        else:
-            self.molecule = molecule
-
-        # init doesn't need to load molecule files
-        if self.__class__.__name__ == 'Init':
-            return
-
-        # assume static inventory if no vagrant config block is defined
-        if self.molecule._provisioner is None:
-            self.static = True
-
-        # assume static inventory if no instances are defined in vagrant config block
-        if self.molecule._provisioner.instances is None:
-            self.static = True
-
-        # Add or update the group_vars if needed.
-        self.molecule._add_or_update_vars('group_vars')
-
-        # Add or update the host_vars if needed
-        self.molecule._add_or_update_vars('host_vars')
-
-        # Update symlinks
-        self.molecule._symlink_vars()
-
-    def disabled(self, cmd):
-        """
-        Prints 'command disabled' message and exits program.
-
-        :param cmd: Name of the disabled command to print.
-        :return: None
-        """
-        errmsg = "The `{}` command isn't supported with static inventory."
-        utilities.logger.error(errmsg.format(cmd))
-        utilities.sysexit()
+    def main(self):
+        c = self.molecule._config
+        if not c.molecule_file_exists():
+            error = '\nUnable to find {}. Exiting.'
+            utilities.logger.error(error.format(c.molecule_file))
+            utilities.sysexit()
+        self.molecule.main()
 
     @abc.abstractproperty
     def execute(self):

molecule/commands/converge.py
@@ -70,10 +70,6 @@ class Converge(base.BaseCommand):
                 create_instances = True
                 create_inventory = True
 
-        if self.static:
-            create_instances = False
-            create_inventory = False
-
         if create_instances and not idempotent:
             command_args, args = utilities.remove_args(self.command_args,
                                                        self.args, ['--tags'])

molecule/commands/create.py
@@ -38,9 +38,6 @@ class Create(base.BaseCommand):
     """
 
     def execute(self, exit=True):
-        if self.static:
-            self.disabled('create')
-
         self.molecule._remove_inventory_file()
         self.molecule._create_templates()
         try:

molecule/commands/destroy.py
@@ -44,9 +44,6 @@ class Destroy(base.BaseCommand):
 
         :return: None
         """
-        if self.static:
-            self.disabled('destroy')
-
         self.molecule._create_templates()
         try:
             utilities.print_info("Destroying instances ...")

molecule/commands/idempotence.py
@@ -37,9 +37,6 @@ class Idempotence(base.BaseCommand):
     """
 
     def execute(self, exit=True):
-        if self.static:
-            self.disabled('idempotence')
-
         utilities.print_info(
             'Idempotence test in progress (can take a few minutes)...')
 

molecule/commands/init.py
@@ -37,6 +37,9 @@ class Init(base.BaseCommand):
         init [<role>] [--docker | --openstack] [--offline]
     """
 
+    def main(self):
+        pass
+
     def clean_meta_main(self, role_path):
         main_path = os.path.join(role_path, 'meta', 'main.yml')
         temp_path = os.path.join(role_path, 'meta', 'main.yml.tmp')

molecule/commands/list.py
@@ -35,9 +35,6 @@ class List(base.BaseCommand):
     """
 
     def execute(self):
-        if self.static:
-            self.disabled('list')
-
         porcelain = self.molecule._args['-m'] or self.molecule._args[
             '--porcelain']
         self.molecule._print_valid_platforms(porcelain=porcelain)

molecule/commands/login.py
@@ -36,9 +36,6 @@ class Login(base.BaseCommand):
     """
 
     def execute(self):
-        if self.static:
-            self.disabled('login')
-
         # Collect the list of running hosts.
         try:
             status = self.molecule._provisioner.status()

molecule/commands/status.py
@@ -42,9 +42,6 @@ class Status(base.BaseCommand):
     """
 
     def execute(self):
-        if self.static:
-            self.disabled('status')
-
         display_all = not any([self.args['--hosts'], self.args['--platforms'],
                                self.args['--providers']])
 

molecule/commands/test.py
@@ -39,9 +39,6 @@ class Test(base.BaseCommand):
     """
 
     def execute(self):
-        if self.static:
-            self.disabled('test')
-
         command_args, args = utilities.remove_args(
             self.command_args, self.args, self.command_args)
 

molecule/commands/verify.py
@@ -44,9 +44,6 @@ class Verify(base.BaseCommand):
     """
 
     def execute(self, exit=True):
-        if self.static:
-            self.disabled('verify')
-
         serverspec_dir = self.molecule._config.config['molecule'][
             'serverspec_dir']
         testinfra_dir = self.molecule._config.config['molecule'][

molecule/config.py
@@ -25,18 +25,18 @@ import anyconfig
 
 from molecule import utilities
 
-CONFIG_PATHS = [os.path.join(
-    os.path.dirname(__file__), 'conf/defaults.yml'), 'molecule.yml',
-                '~/.config/molecule/config.yml']
+DEFAULT_CONFIG = os.path.join(os.path.dirname(__file__), 'conf/defaults.yml')
+PROJECT_CONFIG = 'molecule.yml'
+LOCAL_CONFIG = '~/.config/molecule/config.yml'
 
 
 class Config(object):
-    def __init__(self, configs=CONFIG_PATHS):
+    def __init__(self, configs=[DEFAULT_CONFIG, PROJECT_CONFIG, LOCAL_CONFIG]):
         self.config = self._get_config(configs)
 
     @property
     def molecule_file(self):
-        return 'molecule.yml'
+        return PROJECT_CONFIG
 
     def build_easy_paths(self):
         """
@@ -75,29 +75,16 @@ class Config(object):
         :param platform: platform name to pass to underlying format_instance_name call
         :return: None
         """
-        # assume static inventory if there's no vagrant section
-        if self.config.get('vagrant') is None:
-            return
-
-        # assume static inventory if no instances are listed
-        if self.config['vagrant'].get('instances') is None:
-            return
-
         for instance in self.config['vagrant']['instances']:
             instance['vm_name'] = utilities.format_instance_name(
                 instance['name'], platform,
                 self.config['vagrant']['instances'])
 
-    def _get_config(self, configs):
-        if not self._has_molecule_file():
-            error = '\nUnable to find {}. Exiting.'
-            utilities.logger.error(error.format(self.molecule_file))
-            utilities.sysexit()
-
-        return self._combine(configs)
-
-    def _has_molecule_file(self):
+    def molecule_file_exists(self):
         return os.path.isfile(self.molecule_file)
+
+    def _get_config(self, configs):
+        return self._combine(configs)
 
     def _combine(self, configs):
         """ Perform a prioritized recursive merge of serveral source files,

molecule/core.py
@@ -48,14 +48,8 @@ class Molecule(object):
         self._provisioner = None
 
     def main(self):
-        if self._args.get('<command>') == 'init':
-            return  # exits program
-
-        # ensure the .molecule directory exists
-        if not os.path.isdir(os.path.join(os.curdir, self._config.config[
-                'molecule']['molecule_dir'])):
-            os.mkdir(os.path.join(os.curdir, self._config.config['molecule'][
-                'molecule_dir']))
+        if not os.path.exists(self._config.config['molecule']['molecule_dir']):
+            os.makedirs(self._config.config['molecule']['molecule_dir'])
 
         # concatentate file names and paths within config so they're more convenient to use
         self._config.build_easy_paths()
@@ -85,9 +79,6 @@ class Molecule(object):
             self._print_valid_platforms()
             utilities.sysexit()
 
-        if not os.path.exists(self._config.config['molecule']['molecule_dir']):
-            os.makedirs(self._config.config['molecule']['molecule_dir'])
-
         # updates instances config with full machine names
         self._config.populate_instance_names(self._env['MOLECULE_PLATFORM'])
 
@@ -96,6 +87,9 @@ class Molecule(object):
                             yaml.dump(self._config.config,
                                       default_flow_style=False,
                                       indent=2))
+        self._add_or_update_vars('group_vars')
+        self._add_or_update_vars('host_vars')
+        self._symlink_vars()
 
     def get_provisioner(self):
         if 'vagrant' in self._config.config:

molecule/provisioners/vagrantprovisioner.py
@@ -89,14 +89,6 @@ class VagrantProvisioner(baseprovisioner.BaseProvisioner):
 
     @property
     def default_provider(self):
-        # assume static inventory if there's no vagrant section
-        if self.m._config.config.get('vagrant') is None:
-            return 'static'
-
-        # assume static inventory if no providers are listed
-        if self.m._config.config['vagrant'].get('providers') is None:
-            return 'static'
-
         # take config's default_provider if specified, otherwise use the first in the provider list
         default_provider = self.m._config.config['molecule'].get(
             'default_provider')

tests/conftest.py
@@ -44,12 +44,3 @@ def temp_files(tmpdir, request):
         return confs
 
     return wrapper
-
-
-@pytest.fixture()
-def mock_molecule_file_exists(monkeypatch):
-    def mockreturn(m):
-        return True
-
-    return monkeypatch.setattr('molecule.config.Config._has_molecule_file',
-                               mockreturn)

tests/provisioner/test_dockerprovisioner.py
@@ -51,7 +51,7 @@ def docker_data():
 
 
 @pytest.fixture()
-def molecule_instance(temp_files, docker_data, mock_molecule_file_exists):
+def molecule_instance(temp_files, docker_data):
     c = temp_files(content=[docker_data])
     m = core.Molecule(dict())
     m._config = config.Config(configs=c)

tests/test_config.py
@@ -61,10 +61,19 @@ def config_data():
 
 
 @pytest.fixture()
-def config_instance(temp_files, config_data, mock_molecule_file_exists):
+def config_instance(temp_files, config_data):
     c = temp_files(content=[config_data])
 
     return config.Config(configs=c)
+
+
+@pytest.fixture()
+def mock_molecule_file_exists(monkeypatch):
+    def mockreturn(m):
+        return True
+
+    return monkeypatch.setattr('molecule.config.Config.molecule_file_exists',
+                               mockreturn)
 
 
 def test_molecule_file(config_instance):
@@ -103,17 +112,23 @@ def test_populate_instance_names(config_instance):
         0]['vm_name']
 
 
+def test_molecule_file_exists(temp_files, config_data,
+                              mock_molecule_file_exists):
+    configs = temp_files(content=[config_data])
+    c = config.Config(configs=configs)
+
+    assert c.molecule_file_exists()
+
+
+def test_molecule_file_does_not_exist(config_instance):
+    assert not config_instance.molecule_file_exists()
+
+
 def test_get_config(config_instance):
     assert isinstance(config_instance.config, dict)
 
 
-def test_get_config_raises_when_missing_molecule_file():
-    with pytest.raises(SystemExit):
-        config.Config()
-
-
-def test_combine_default_config(temp_files, default_config_data,
-                                mock_molecule_file_exists):
+def test_combine_default_config(temp_files, default_config_data):
     c = temp_files(content=[default_config_data])
     config_instance = config.Config(configs=c).config
 
@@ -122,8 +137,7 @@ def test_combine_default_config(temp_files, default_config_data,
 
 
 def test_combine_project_config_overrides_default_config(
-        temp_files, default_config_data, project_config_data,
-        mock_molecule_file_exists):
+        temp_files, default_config_data, project_config_data):
     c = temp_files(content=[default_config_data, project_config_data])
     config_instance = config.Config(configs=c).config
 
@@ -133,7 +147,7 @@ def test_combine_project_config_overrides_default_config(
 
 def test_combine_local_config_overrides_default_and_project_config(
         temp_files, default_config_data, project_config_data,
-        local_config_data, mock_molecule_file_exists):
+        local_config_data):
     c = temp_files(
         content=[default_config_data, project_config_data, local_config_data])
     config_instance = config.Config(configs=c).config

tests/test_core.py
@@ -24,7 +24,7 @@ from molecule import core
 
 
 @pytest.fixture()
-def molecule(mock_molecule_file_exists):
+def molecule():
     return core.Molecule(dict())
 
 