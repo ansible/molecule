f6afd69d060dfb5ef783e85e909e95b8313fbba9
Adam Brown <adambr2@cisco.com>
2016-07-28 18:05:52 -0500

    allow flake8 to ignore this long line

--- molecule/provisioners/dockerprovisioner.py
+++ molecule/provisioners/dockerprovisioner.py
@@ -113,7 +113,7 @@ class DockerProvisioner(baseprovisioner.BaseProvisioner):
             dockerfile = '''
             FROM {}:{}
             RUN bash -c 'if [ -x "$(command -v apt-get)" ]; then apt-get update && apt-get install -y python sudo; fi'
-            RUN bash -c 'if [ -x "$(command -v yum)" ]; then yum makecache fast && yum update -y && yum install -y python sudo; fi'
+            RUN bash -c 'if [ -x "$(command -v yum)" ]; then yum makecache fast && yum update -y && yum install -y python sudo; fi'  # NOQA
 
             '''  # noqa
 
