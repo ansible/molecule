d6210b0504668e556b2b21d207dbfc855e6c14bb
Tim Colbert <tim.colbert@itoc.com.au>
2016-12-09 11:54:15 +1000

    Add yum-plugin-ovl to support overlayfs in containers

--- molecule/driver/dockerdriver.py
+++ molecule/driver/dockerdriver.py
@@ -233,7 +233,7 @@ class DockerDriver(basedriver.BaseDriver):
             FROM {container_image}:{container_version}
             {container_environment}
             RUN bash -c 'if [ -x "$(command -v apt-get)" ]; then apt-get update && apt-get install -y python sudo; fi'
-            RUN bash -c 'if [ -x "$(command -v yum)" ]; then yum makecache fast && yum update -y && yum install -y python sudo; fi'
+            RUN bash -c 'if [ -x "$(command -v yum)" ]; then yum makecache fast && yum update -y && yum install -y python sudo yum-plugin-ovl; fi'
             RUN bash -c 'if [ -x "$(command -v zypper)" ]; then zypper refresh && zypper update -y && zypper install -y python sudo; fi'
 
             '''  # noqa
