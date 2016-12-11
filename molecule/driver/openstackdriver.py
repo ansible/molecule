ef02c8ea44eb5447187804479c81859d8b245237
Alvaro Aleman <alv2412@googlemail.com>
2016-12-11 14:45:02 +0100

    Openstack: Fix security_groups default for newer shade versions

--- molecule/driver/openstackdriver.py
+++ molecule/driver/openstackdriver.py
@@ -120,8 +120,7 @@ class OpenstackDriver(basedriver.BaseDriver):
                     auto_ip=True,
                     wait=True,
                     key_name=kpn,
-                    security_groups=instance['security_groups']
-                    if 'security_groups' in instance else None)
+                    security_groups=instance.get('security_groups', []))
                 self._reset_known_host_key(server['interface_ip'])
                 instance['created'] = True
                 num_retries = 0
