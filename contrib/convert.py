b7d7740db482624182dd6c31600ca1c09669cfc5
John Dewey <john@dewey.ws>
2017-08-03 11:51:10 -0700

    Moved convert to click vs argparse
    As part of moving to click, reworked convert into a class.

--- contrib/convert.py
+++ contrib/convert.py
@@ -20,12 +20,12 @@
 #  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 #  DEALINGS IN THE SOFTWARE.
 
-import argparse
 import errno
 import glob
 import os
 import shutil
 
+import click
 import sh
 
 from molecule import logger
@@ -34,63 +34,96 @@ from molecule import util
 
 LOG = logger.get_logger(__name__)
 
-parser = argparse.ArgumentParser()
-parser.add_argument('old_molecule_file')
-args = parser.parse_args()
 
-old_molecule_file = args.old_molecule_file
+class Convert(object):
+    def __init__(self, old_molecule_file, driver_name):
+        self._old_molecule_file = old_molecule_file
 
-if not os.path.isfile(old_molecule_file):
-    msg = 'Unable to find {}. Exiting.'.format(old_molecule_file)
-    util.sysexit_with_message(msg)
+        if not os.path.isfile(self._old_molecule_file):
+            msg = 'Unable to find {}. Exiting.'.format(self._old_molecule_file)
+            util.sysexit_with_message(msg)
 
-m = migrate.Migrate(old_molecule_file)
-old_role_dir = os.path.join(os.path.dirname(old_molecule_file))
-old_dot_molecule_dir = os.path.join(old_role_dir, '.molecule')
-old_test_dir = os.path.join(old_role_dir, 'tests')
-old_playbook = os.path.join(old_role_dir, 'playbook.yml')
-molecule_dir = os.path.join(old_role_dir, 'molecule')
-scenario_dir = os.path.join(molecule_dir, 'default')
-test_dir = os.path.join(scenario_dir, 'tests')
-molecule_file = os.path.join(scenario_dir, 'molecule.yml')
-role_name = os.path.basename(os.path.normpath(old_role_dir))
+        self._m = migrate.Migrate(self._old_molecule_file)
+        self._old_role_dir = os.path.join(
+            os.path.dirname(self._old_molecule_file))
+        self._old_dot_molecule_dir = os.path.join(self._old_role_dir,
+                                                  '.molecule')
+        self._old_test_dir = os.path.join(self._old_role_dir, 'tests')
+        self._old_playbook = os.path.join(self._old_role_dir, 'playbook.yml')
+        self._molecule_dir = os.path.join(self._old_role_dir, 'molecule')
+        self._scenario_dir = os.path.join(self._molecule_dir, 'default')
+        self._test_dir = os.path.join(self._scenario_dir, 'tests')
+        self._molecule_file = os.path.join(self._scenario_dir, 'molecule.yml')
+        self._role_name = os.path.basename(
+            os.path.normpath(self._old_role_dir))
 
-options = {
-    'role_name': role_name,
-    'scenario_name': 'default',
-    'driver_name': 'vagrant',
-}
-cmd = sh.molecule.bake('init', 'scenario', _cwd=old_role_dir, **options)
-util.run_command(cmd)
+    def migrate(self):
+        self._create_scenario()
+        self._create_new_molecule_file()
+        self._copy_old_files()
+        self._cleanup_old_files()
 
-with open(molecule_file, 'w') as stream:
-    msg = 'Writing molecule.yml to {}'.format(molecule_file)
-    LOG.info(msg)
-    stream.write(m.dump())
+    def _create_scenario(self):
+        options = {
+            'role_name': self._role_name,
+            'scenario_name': 'default',
+            'driver_name': 'vagrant',
+        }
+        cmd = sh.molecule.bake(
+            'init', 'scenario', _cwd=self._old_role_dir, **options)
+        util.run_command(cmd)
 
-for f in glob.glob(r'{}/test_*.py'.format(old_test_dir)):
-    msg = 'Copying {} to {}'.format(f, test_dir)
-    LOG.info(msg)
-    shutil.copy(f, test_dir)
+    def _create_new_molecule_file(self):
+        with open(self._molecule_file, 'w') as stream:
+            msg = 'Writing molecule.yml to {}'.format(self._molecule_file)
+            LOG.info(msg)
+            stream.write(self._m.dump())
 
-if os.path.isfile(old_playbook):
-    msg = 'Copying {} to {}'.format(old_playbook, scenario_dir)
-    LOG.info(msg)
-    shutil.copy(old_playbook, scenario_dir)
+    def _copy_old_files(self):
+        for f in glob.glob(r'{}/test_*.py'.format(self._old_test_dir)):
+            msg = 'Copying {} to {}'.format(f, self._test_dir)
+            LOG.info(msg)
+            shutil.copy(f, self._test_dir)
 
-files = [
-    old_dot_molecule_dir,
-    old_molecule_file,
-    old_playbook,
-]
-for f in files:
-    if os.path.exists(f):
-        msg = 'Deleting {}'.format(f)
-        LOG.warn(msg)
-        try:
-            shutil.rmtree(f)
-        except OSError as exc:
-            if exc.errno == errno.ENOTDIR:
-                os.remove(f)
-            else:
-                raise
+        if os.path.isfile(self._old_playbook):
+            msg = 'Copying {} to {}'.format(self._old_playbook,
+                                            self._scenario_dir)
+            LOG.info(msg)
+            shutil.copy(self._old_playbook, self._scenario_dir)
+
+    def _cleanup_old_files(self):
+        files = [
+            self._old_dot_molecule_dir,
+            self._old_molecule_file,
+            self._old_playbook,
+        ]
+        for f in files:
+            if os.path.exists(f):
+                msg = 'Deleting {}'.format(f)
+                LOG.warn(msg)
+                try:
+                    shutil.rmtree(f)
+                except OSError as exc:
+                    if exc.errno == errno.ENOTDIR:
+                        os.remove(f)
+                    else:
+                        raise
+
+
+@click.command()
+@click.argument('old_molecule_file', required=True)
+@click.option(
+    '--driver-name',
+    '-d',
+    type=click.Choice(['vagrant']),
+    default='vagrant',
+    help='Name of driver to migrate. (vagrant)')
+def main(old_molecule_file, driver_name):  # pragma: no cover
+    """ Migrate a Molecule v1 role to the v2 format. """
+
+    c = Convert(old_molecule_file, driver_name)
+    c.migrate()
+
+
+if __name__ == '__main__':
+    main()
