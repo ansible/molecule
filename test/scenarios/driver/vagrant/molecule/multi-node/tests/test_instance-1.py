bdd82fb2a2a8dff38f00e0d5b866194e8414871f
John Dewey <john@dewey.ws>
2018-03-09 09:34:42 -0800

    Vagrantfile cleanup
    Cleanup the config and allow the developer to change options on the
    base vagrant config object.
    
    Fixes: #1170

---
+++ test/scenarios/driver/vagrant/molecule/multi-node/tests/test_instance-1.py
@@ -0,0 +1,31 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('instance-1')
+
+
+def test_distro(host):
+    f = host.file('/etc/debian_version')
+
+    assert f.is_file
+
+
+def test_cpus(host):
+    cpus = host.ansible("setup")['ansible_facts']['ansible_processor_vcpus']
+
+    assert 1 == int(cpus)
+
+
+def test_memory(host):
+    total_memory = host.ansible("setup")['ansible_facts'][
+        'ansible_memtotal_mb']
+
+    assert (1024 / 2) <= int(total_memory) <= 1024
+
+
+def test_has_shared_directory(host):
+    f = host.file('/vagrant')
+
+    assert f.is_directory
