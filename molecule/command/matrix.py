e7a9c87139089722759eee24e31b69341b2eb659
įoнииγ įoнииγ <john@dewey.ws>
2018-07-04 12:53:42 -0700

    Added matrix subcommand (#1366)
    Will display actions Molecule will take for a given
    scenario/subcommand.
    
    Fixes: #1337

---
+++ molecule/command/matrix.py
@@ -0,0 +1,85 @@
+#  Copyright (c) 2015-2018 Cisco Systems, Inc.
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to
+#  deal in the Software without restriction, including without limitation the
+#  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+#  sell copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+#  DEALINGS IN THE SOFTWARE.
+
+import click
+
+from molecule import logger
+from molecule import scenarios
+from molecule.command import base
+
+LOG = logger.get_logger(__name__)
+
+
+class Matrix(base.Base):
+    """
+    .. program:: molecule matrix subcommand
+
+    .. option:: molecule matrix subcommand
+
+        Target the default scenario.
+
+    .. program:: molecule matrix --scenario-name foo subcommand
+
+    .. option:: molecule matrix --scenario-name foo subcommand
+
+        Targeting a specific scenario.
+
+    .. program:: molecule --debug matrix subcommand
+
+    .. option:: molecule --debug matrix subcommand
+
+        Executing with `debug`.
+
+    .. program:: molecule --base-config base.yml matrix subcommand
+
+    .. option:: molecule --base-config base.yml matrix subcommand
+
+        Executing with a `base-config`.
+
+    .. program:: molecule --env-file foo.yml matrix subcommand
+
+    .. option:: molecule --env-file foo.yml matrix subcommand
+
+        Load an env file to read variables from when rendering
+        molecule.yml.
+    """
+
+
+@click.command()
+@click.pass_context
+@click.option(
+    '--scenario-name',
+    '-s',
+    default=base.MOLECULE_DEFAULT_SCENARIO_NAME,
+    help='Name of the scenario to target. ({})'.format(
+        base.MOLECULE_DEFAULT_SCENARIO_NAME))
+@click.argument('subcommand', nargs=-1, type=click.UNPROCESSED)
+def matrix(ctx, scenario_name, subcommand):  # pragma: no cover
+    """
+    List matrix of steps used to test instances.
+    """
+    args = ctx.obj.get('args')
+    command_args = {
+        'subcommand': subcommand[0],
+    }
+
+    s = scenarios.Scenarios(
+        base.get_configs(args, command_args, scenario_name))
+    s.print_matrix()
