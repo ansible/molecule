406ea840ad13d198e4264750113878120379c6c8
wilmardo <info@wilmardenouden.nl>
2018-09-28 10:02:52 +0200

    feat(lxd): Adds a little elaboration about lxd_container module LXD API

--- molecule/driver/lxd.py
+++ molecule/driver/lxd.py
@@ -31,8 +31,11 @@ class LXD(base.Base):
 
     Molecule leverages Ansible's `lxd_container`_ module, by mapping variables
     from `molecule.yml` into `create.yml` and `destroy.yml`.
+    The `lxd_container`_ module leverages the LXD API. Usefull information
+    about, for example the source variable can be found in the `LXD API documentation`.
 
     .. _`lxd_container`: http://docs.ansible.com/ansible/latest/lxd_container_module.html
+    .. _`LXD API documentation`: https://github.com/lxc/lxd/blob/master/doc/rest-api.md#post-1
 
     .. code-block:: yaml
 
