da8fa6673b1f6cd5f98c7aab293bff296bdc6123
Aliaksei <zelalexey@gmail.com>
2019-01-17 14:02:19 +0300

    refactoring ansible_connection_options func for winrm support
    Signed-off-by: Aliaksei <zelalexey@gmail.com>

--- molecule/driver/delegated.py
+++ molecule/driver/delegated.py
@@ -150,36 +150,19 @@ class Delegated(base.Base):
         if self.managed:
             try:
                 d = self._get_instance_config(instance_name)
+                conn_dict = {}
+                conn_dict['ansible_user'] = d.get('user')
+                conn_dict['ansible_host'] = d.get('address')
+                conn_dict['ansible_port'] = d.get('port')
+                conn_dict['ansible_connection'] = d.get('connection', 'smart')
+                if d.get('identity_file'):
+                    conn_dict['ansible_private_key_file'] = d.get(
+                        'identity_file')
+                    conn_dict['ansible_ssh_common_args'] = ' '.join(
+                        self.ssh_connection_options)
 
-                if d['ansible_connection'] == "winrm":
-                    return {
-                        'ansible_user':
-                            d['user'],
-                        'ansible_host':
-                            d['address'],
-                        'ansible_port':
-                            d['port'],
-                        'ansible_winrm_transport':
-                            d['ansible_winrm_transport'],
-                        'ansible_connection':
-                            d['ansible_connection'],
-                        'ansible_winrm_server_cert_validation': 'ignore'
-                    }
-                else:
-                    return {
-                        'ansible_user':
-                        d['user'],
-                        'ansible_host':
-                        d['address'],
-                        'ansible_port':
-                        d['port'],
-                        'ansible_private_key_file':
-                        d['identity_file'],
-                        'connection':
-                        'ssh',
-                        'ansible_ssh_common_args':
-                        ' '.join(self.ssh_connection_options),
-                    }
+                return conn_dict
+
             except StopIteration:
                 return {}
             except IOError:
