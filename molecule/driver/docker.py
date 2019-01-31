899967fe1370fb2ec3467e5521e40df88ddd495d
John R Barker <john@johnrbarker.com>
2019-01-31 16:10:43 +0000

    Fix all links (#1720)
    Lots of domains redirect to https, so use that preferred URL.
    Ensure for docs.ansible.com/ansible we are using /latest/

--- molecule/driver/docker.py
+++ molecule/driver/docker.py
@@ -36,7 +36,7 @@ class Docker(base.Base):
     Molecule leverages Ansible's `docker_container`_ module, by mapping
     variables from ``molecule.yml`` into ``create.yml`` and ``destroy.yml``.
 
-    .. _`docker_container`: http://docs.ansible.com/ansible/latest/docker_container_module.html
+    .. _`docker_container`: https://docs.ansible.com/ansible/latest/docker_container_module.html
     .. _`Docker Security Configuration`: https://docs.docker.com/engine/reference/run/#security-configuration
 
     .. code-block:: yaml
