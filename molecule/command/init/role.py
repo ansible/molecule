13db14cc9f247fd2d62ead5566640ce5c93216a1
Alexis Lessard <alexislessard95@gmail.com>
2019-02-09 22:14:18 -0500

    Cleans the code according to linting rules
    Signed-off-by: Alexis Lessard <alexislessard95@gmail.com>

--- molecule/command/init/role.py
+++ molecule/command/init/role.py
@@ -43,11 +43,10 @@ class Role(base.Base):
 
     .. option:: molecule init role --role-name foo --template path
 
-        Initialize a new role using a local *cookiecutter* template. This allows
-        the customization of a role while still using the ``molecule`` folder, 
-        wich you can still customize with all the ``init role`` options. This
-        is similar to an ``ansible-galaxy init`` skeleton.
-
+        Initialize a new role using a local *cookiecutter* template. This
+        allows the customization of a role while still using the ``molecule``
+        folder, wich you can still customize with all the ``init role``
+        options. This is similar to an ``ansible-galaxy init`` skeleton.
     """
 
     def __init__(self, command_args):
