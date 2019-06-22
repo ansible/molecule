b44fe7174c9e094b5f122a7511161154a5b6505e
Sorin Sbarnea <ssbarnea@users.noreply.github.com>
2019-06-22 10:13:30 +0100

    Refactored ephemeral directory use (#2089)
    Avoid hardcoded temporary directories which can create conflicts with
    with multi-user environments.
    
    Signed-off-by: Sorin Sbarnea <ssbarnea@redhat.com>
    Fixes: #1506

--- test/unit/test_scenario.py
+++ test/unit/test_scenario.py
@@ -20,7 +20,6 @@
 
 import os
 import shutil
-import tempfile
 
 import pytest
 
@@ -109,13 +108,7 @@ def test_directory_property(molecule_scenario_directory_fixture, _instance):
 
 
 def test_ephemeral_directory_property(_instance):
-    project_directory = os.path.basename(_instance.config.project_directory)
-    scenario_name = _instance.name
-    project_scenario_directory = os.path.join('molecule', project_directory,
-                                              scenario_name)
-    e_dir = os.path.join(tempfile.gettempdir(), project_scenario_directory)
-
-    assert e_dir == _instance.ephemeral_directory
+    assert os.access(_instance.ephemeral_directory, os.W_OK)
 
 
 def test_inventory_directory_property(_instance):
@@ -235,20 +228,18 @@ def test_setup_creates_ephemeral_and_inventory_directories(_instance):
 
 
 def test_ephemeral_directory():
-    e_dir = os.path.join(tempfile.gettempdir(), 'foo/bar')
-
-    assert e_dir == scenario.ephemeral_directory('foo/bar')
+    # assure we can write to ephemeral directory
+    assert os.access(scenario.ephemeral_directory('foo/bar'), os.W_OK)
 
 
 def test_ephemeral_directory_overriden_via_env_var(monkeypatch):
     monkeypatch.setenv('MOLECULE_EPHEMERAL_DIRECTORY', 'foo/bar')
-    e_dir = os.path.join(tempfile.gettempdir(), 'foo/bar')
 
-    assert e_dir == scenario.ephemeral_directory('foo/bar')
+    assert os.access(scenario.ephemeral_directory('foo/bar'), os.W_OK)
 
 
 def test_ephemeral_directory_overriden_via_env_var_uses_absolute_path(
         monkeypatch):
-    monkeypatch.setenv('MOLECULE_EPHEMERAL_DIRECTORY', '/foo/bar')
+    monkeypatch.setenv('MOLECULE_EPHEMERAL_DIRECTORY', "foo/bar")
 
-    assert '/foo/bar' == scenario.ephemeral_directory('foo/bar')
+    assert os.path.isabs(scenario.ephemeral_directory())
