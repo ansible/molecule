68a70cdf5990b02d86a95cca9f3948285b36485f
flow.gunso <flow.gunso@gmail.com>
2019-08-31 22:51:35 +0200

    Rephrase, remove false statement, refer to Docker documentation
    Signed-off-by: flow.gunso <flow.gunso@gmail.com>

--- molecule/driver/docker.py
+++ molecule/driver/docker.py
@@ -39,6 +39,7 @@ class Docker(base.Base):
 
     .. _`docker_container`: https://docs.ansible.com/ansible/latest/docker_container_module.html
     .. _`Docker Security Configuration`: https://docs.docker.com/engine/reference/run/#security-configuration
+    .. _`Docker daemon socket options`: https://docs.docker.com/engine/reference/commandline/dockerd/#daemon-socket-option
 
     .. code-block:: yaml
 
@@ -119,10 +120,11 @@ class Docker(base.Base):
               less secure. For details, please reference `Docker Security
               Configuration`_
 
-    .. note:: With the environment variable ``DOCKER_HOST`` the user can define
-              which `Docker`_ daemon to use. Do note that the `Docker`_ binary
-              must be installed on the machine that run Molecule even if the 
-              `Docker`_ instance is remote.
+    .. note:: With the environment variable ``DOCKER_HOST`` the user can bind
+              Molecule to a different `Docker`_ socket than the default 
+              ``unix:///var/run/docker.sock``. ``tcp``, ``fd`` and ``ssh``
+              socket types can be configured. For details, please reference 
+              `Docker daemon socket options`_.
 
     .. code-block:: yaml
 
