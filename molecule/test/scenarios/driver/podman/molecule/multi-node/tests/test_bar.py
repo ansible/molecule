76858f20cee3376fbeedb2e167d7f717a67c8b4e
Sergey <sshnaidm@users.noreply.github.com>
2019-10-24 13:31:23 +0300

    Add podman functional tests (#2323)
    Run podman tests for molecule podman driver
    Closes #2159
    Signed-off-by: Sagi Shnaidman <sshnaidm@redhat.com>

---
+++ molecule/test/scenarios/driver/podman/molecule/multi-node/tests/test_bar.py
@@ -0,0 +1,30 @@
+import re
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']
+).get_hosts('bar')
+
+
+def test_hostname(host):
+    assert re.search(r'instance-1.*', host.check_output('hostname -s'))
+
+
+def test_etc_molecule_directory(host):
+    f = host.file('/etc/molecule')
+
+    assert f.is_directory
+    assert f.user == 'root'
+    assert f.group == 'root'
+    assert f.mode == 0o755
+
+
+def test_etc_molecule_ansible_hostname_file(host):
+    f = host.file('/etc/molecule/{}'.format(host.check_output('hostname -s')))
+
+    assert f.is_file
+    assert f.user == 'root'
+    assert f.group == 'root'
+    assert f.mode == 0o644
