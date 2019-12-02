57212defeebb291029396cbc2736bd5514573230
Sorin Sbarnea <ssbarnea@redhat.com>
2019-12-02 12:26:59 +0000

    travis: prevent accidents with pytest skips on CI
    Introduces a PYTEST_REQPASS option that makes pytest fail if the
    number of passed tests does not exactly match number its value.
    
    This should prevent accidents where some tests are not run due to other
    changes made to the test environment.
    
    We had at least one case where `--delegated` feature prevented
    execution of some tests in CI and we ended up with more broken tests.
    
    This change should not affect non-CI execution because failure happens
    only when this variable is defined.
    
    Signed-off-by: Sorin Sbarnea <ssbarnea@redhat.com>

--- conftest.py
+++ conftest.py
@@ -1,5 +1,7 @@
+from __future__ import print_function
 import os
 import pytest
+import sys
 
 pytest_plugins = ['helpers_namespace']
 
@@ -14,3 +16,22 @@ def environ():
     # adds extra environment variables that may be needed during testing
     if not os.environ.get('TEST_BASE_IMAGE', ""):
         os.environ['TEST_BASE_IMAGE'] = 'docker.io/pycontribs/centos:7'
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_terminal_summary(terminalreporter, exitstatus, config):
+    yield
+    req_passed = int(os.environ.get('PYTEST_REQPASS', '0'))
+    if req_passed:
+        passed = 0
+        for x in terminalreporter.stats.get('passed', []):
+            if x.when == 'call' and x.outcome == 'passed':
+                passed += 1
+        if passed != req_passed:
+            print(
+                'ERROR: {} passed test but expected number was {}.'.format(
+                    passed, req_passed
+                ),
+                file=sys.stderr,
+            )
+            sys.exit(127)
