f2b1c36a068288fbc0273fbf48e30c5f6ef45651
Kevin Gentile <kgentile@lb.com>
2020-04-20 09:43:13 -0400

    add dev entrypoint

--- tools/entrypoint.sh
+++ tools/entrypoint.sh
@@ -4,4 +4,22 @@
 pip3 install -e .
 
 # start docker daemon
-dockerd-entrypoint.sh &>/var/log/dockerd.log
+echo "Starting docker daemon..."
+dockerd-entrypoint.sh &> /var/log/dockerd.log &
+
+echo "Waiting for docker daemon..."
+retry=0
+timeout=20 #sec
+docker info &> /dev/null
+while [ $? -eq 1 ]
+    do
+    if [ $retry -gt  $timeout ]; then
+        echo "Daemon failed to spawn after $timeout sec"
+        exit
+    fi
+    sleep 1
+    ((retry++))
+    docker info &> /dev/null
+done
+
+exec $@
