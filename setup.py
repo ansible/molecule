ef812c3d1ec3152b0d8cd854f3c030d2441bde3f
Sorin Sbarnea <ssbarnea@redhat.com>
2020-07-07 12:50:42 +0100

    Updated version packaging
    Avoid occasional error which may confuse dist tools.

--- setup.py
+++ setup.py
@@ -32,5 +32,5 @@ site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
 if __name__ == "__main__":
     setuptools.setup(
         use_scm_version={"local_scheme": "no-local-version"},
-        setup_requires=["setuptools_scm"],
+        setup_requires=["setuptools_scm[toml]>=3.5.0"],
     )
