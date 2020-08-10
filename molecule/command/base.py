1de5946f606ab16be168c29eec7e8eb687a9698f
Sorin Sbarnea <ssbarnea@users.noreply.github.com>
2020-08-10 10:07:47 +0100

    Avoid abridged click output (#2768)
    * Avoid abridged click output
    
    Fixes: #2733
    Related: https://github.com/pallets/click/issues/486
    
    * Update base.py

--- molecule/command/base.py
+++ molecule/command/base.py
@@ -216,6 +216,9 @@ def click_group_ex():
     # yellow : special commands, like full test sequence, or login
     return click.group(
         cls=HelpColorsGroup,
+        # Workaround to disable click help line truncation to ~80 chars
+        # https://github.com/pallets/click/issues/486
+        context_settings=dict(max_content_width=9999),
         help_headers_color="yellow",
         help_options_color="green",
         help_options_custom_colors={
