7f2f24e84efc5d34010cbda63fd75c3ba2b23e46
Jack <jack4zhang@gmail.com>
2023-07-31 21:19:30 +0800

    Avoid reinstalling dependencies (#3979)
    With this PR, if dependencies exist, we don't need to talk to
    `galaxy.ansible.com` every time by default.
    
    Technically, it can be done via an explicit definition as below.
    
    ```yaml
    dependency:
      name: galaxy
      options:
        requirements-file: requirements.yml
        force: false
    ```
    
    ## Compare
    
    If we define `community.docker` as a dependency,
    
    ### Before
    
    ```log
    Opened /Users/jackzhang/.ansible/galaxy_token
    Collection 'community.docker:3.4.8' obtained from server default https://galaxy.ansible.com/api/
    Starting galaxy collection install process
    Process install dependency map
    Starting collection install process
    Downloading https://galaxy.ansible.com/download/community-docker-3.4.8.tar.gz to /Users/jackzhang/.ansible/tmp/ansible-local-908270jolbbei/tmp8p2pq2qd/community-docker-3.4.8-ktwznndg
    Installing 'community.docker:3.4.8' to '/Users/jackzhang/.ansible/collections/ansible_collections/community/docker'
    community.docker:3.4.8 was installed successfully
    INFO     Dependency completed successfully.
    WARNING  Skipping, missing the requirements file.
    ```
    
    ### After
    
    ```log
    Starting galaxy collection install process
    Nothing to do. All requested collections are already installed. If you want to reinstall them, consider using `--force`.
    INFO     Dependency completed successfully.
    WARNING  Skipping, missing the requirements file.
    ```
    
    Signed-off-by: Jack Zhang <jack4zhang@gmail.com>

--- src/molecule/test/a_unit/dependency/ansible_galaxy/test_collections.py
+++ src/molecule/test/a_unit/dependency/ansible_galaxy/test_collections.py
@@ -70,7 +70,7 @@ def test_config_private_member(_instance):
 
 
 def test_default_options_property(_instance, role_file):
-    x = {"requirements-file": role_file, "force": True}
+    x = {"requirements-file": role_file, "force": False}
 
     assert x == _instance.default_options
 
@@ -95,7 +95,7 @@ def test_enabled_property(_instance):
 @pytest.mark.parametrize("config_instance", ["_dependency_section_data"], indirect=True)
 def test_options_property(_instance, role_file):
     x = {
-        "force": True,
+        "force": False,
         "requirements-file": role_file,
         "foo": "bar",
         "v": True,
@@ -108,7 +108,7 @@ def test_options_property(_instance, role_file):
 def test_options_property_handles_cli_args(role_file, _instance):
     _instance._config.args = {"debug": True}
     x = {
-        "force": True,
+        "force": False,
         "requirements-file": role_file,
         "foo": "bar",
         "vvv": True,
@@ -131,7 +131,6 @@ def test_collections_bake(_instance, role_file):
         "install",
         "--foo",
         "bar",
-        "--force",
         "--requirements-file",
         role_file,
         "-v",
