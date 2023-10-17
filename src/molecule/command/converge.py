428df30066bcf89be57774154d9547290ad7e00a
Et7f3 <cadeaudeelie@gmail.com>
2023-10-17 22:13:24 +0000

    typo add comma

--- src/molecule/command/converge.py
+++ src/molecule/command/converge.py
@@ -52,7 +52,7 @@ class Converge(base.Base):
 )
 @click.argument("ansible_args", nargs=-1, type=click.UNPROCESSED)
 def converge(ctx, scenario_name, ansible_args):  # pragma: no cover
-    """Use the provisioner to configure instances (dependency, create, prepare converge)."""
+    """Use the provisioner to configure instances (dependency, create, prepare, converge)."""
     args = ctx.obj.get("args")
     subcommand = base._get_subcommand(__name__)
     command_args = {"subcommand": subcommand}
