031c4e117db93a287324b83a70fef0e2ac80c5e6
Bradley A. Thornton <bthornto@redhat.com>
2024-06-27 11:37:11 -0700

    not a commit message

--- src/molecule/command/idempotence.py
+++ src/molecule/command/idempotence.py
@@ -71,7 +71,7 @@ class Idempotence(base.Base):
         # Look for any non-zero changed lines
         changed = re.search(r"(changed=[1-9][0-9]*)", output)
 
-        return bool(changed)
+        return not bool(changed)
 
     def _non_idempotent_tasks(self, output):  # type: ignore[no-untyped-def]  # noqa: ANN001, ANN202
         """Parse the output to identify the non idempotent tasks.
