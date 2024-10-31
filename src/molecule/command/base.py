75f762fa3813c90e86fa989d1973987ba85b5a77
Christian Fasnacht <public-github@fasnacht.it>
2024-10-31 16:29:24 +0100

    Add -h option to print help (#4316)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/molecule/command/base.py
+++ src/molecule/command/base.py
@@ -326,7 +326,11 @@ def click_group_ex() -> ClickGroup:
         cls=HelpColorsGroup,
         # Workaround to disable click help line truncation to ~80 chars
         # https://github.com/pallets/click/issues/486
-        context_settings={"max_content_width": 9999, "color": should_do_markup()},
+        context_settings={
+            "max_content_width": 9999,
+            "color": should_do_markup(),
+            "help_option_names": ["-h", "--help"],
+        },
         help_headers_color="yellow",
         help_options_color="green",
         help_options_custom_colors={
