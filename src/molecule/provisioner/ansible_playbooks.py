f267312cd934635f2a04cad60adff1ee97b74ead
Kate Case <kcase@redhat.com>
2024-11-01 12:06:34 -0400

    Add docstrings and type hints to ansible_playbooks

--- src/molecule/provisioner/ansible_playbooks.py
+++ src/molecule/provisioner/ansible_playbooks.py
@@ -23,7 +23,26 @@ from __future__ import annotations
 import logging
 import os
 
+from pathlib import Path
+from typing import TYPE_CHECKING
+
 from molecule import util
+
+
+if TYPE_CHECKING:
+    from typing import Literal
+
+    from molecule.config import Config
+
+    Section = Literal[
+        "cleanup",
+        "create",
+        "converge",
+        "destroy",
+        "prepare",
+        "side_effect",
+        "verify",
+    ]
 
 
 LOG = logging.getLogger(__name__)
@@ -32,8 +51,8 @@ LOG = logging.getLogger(__name__)
 class AnsiblePlaybooks:
     """A class to act as a module to namespace playbook properties."""
 
-    def __init__(self, config) -> None:  # type: ignore[no-untyped-def]  # noqa: ANN001
-        """Initialize a new namespace class and returns None.
+    def __init__(self, config: Config) -> None:
+        """Initialize a new namespace class.
 
         Args:
             config: An instance of a Molecule config.
@@ -41,61 +60,108 @@ class AnsiblePlaybooks:
         self._config = config
 
     @property
-    def cleanup(self):  # type: ignore[no-untyped-def]  # noqa: ANN201, D102
-        return self._get_playbook("cleanup")  # type: ignore[no-untyped-call]
+    def cleanup(self) -> str | None:
+        """Get the cleanup playbook path.
+
+        Returns:
+            Path to cleanup.yml.
+        """
+        return self._get_playbook("cleanup")
 
     @property
-    def create(self):  # type: ignore[no-untyped-def]  # noqa: ANN201, D102
-        return self._get_playbook("create")  # type: ignore[no-untyped-call]
+    def create(self) -> str | None:
+        """Get the create playbook path.
+
+        Returns:
+            Path to create.yml.
+        """
+        return self._get_playbook("create")
 
     @property
-    def converge(self):  # type: ignore[no-untyped-def]  # noqa: ANN201, D102
-        return self._get_playbook("converge")  # type: ignore[no-untyped-call]
+    def converge(self) -> str | None:
+        """Get the converge playbook path.
+
+        Returns:
+            Path to converge.yml.
+        """
+        return self._get_playbook("converge")
 
     @property
-    def destroy(self):  # type: ignore[no-untyped-def]  # noqa: ANN201, D102
-        return self._get_playbook("destroy")  # type: ignore[no-untyped-call]
+    def destroy(self) -> str | None:
+        """Get the destroy playbook path.
+
+        Returns:
+            Path to destroy.yml.
+        """
+        return self._get_playbook("destroy")
 
     @property
-    def prepare(self):  # type: ignore[no-untyped-def]  # noqa: ANN201, D102
-        return self._get_playbook("prepare")  # type: ignore[no-untyped-call]
+    def prepare(self) -> str | None:
+        """Get the prepare playbook path.
+
+        Returns:
+            Path to prepare.yml.
+        """
+        return self._get_playbook("prepare")
 
     @property
-    def side_effect(self):  # type: ignore[no-untyped-def]  # noqa: ANN201, D102
-        return self._get_playbook("side_effect")  # type: ignore[no-untyped-call]
+    def side_effect(self) -> str | None:
+        """Get the side_effect playbook path.
+
+        Returns:
+            Path to side_effect.yml.
+        """
+        return self._get_playbook("side_effect")
 
     @property
-    def verify(self):  # type: ignore[no-untyped-def]  # noqa: ANN201, D102
-        return self._get_playbook("verify")  # type: ignore[no-untyped-call]
+    def verify(self) -> str | None:
+        """Get the verify playbook path.
 
-    def _get_playbook_directory(self):  # type: ignore[no-untyped-def]  # noqa: ANN202
-        return util.abs_path(
-            os.path.join(self._config.provisioner.directory, "playbooks"),  # noqa: PTH118
-        )
+        Returns:
+            Path to verify.yml.
+        """
+        return self._get_playbook("verify")
 
-    def _get_playbook(self, section):  # type: ignore[no-untyped-def]  # noqa: ANN001, ANN202
+    def _get_playbook_directory(self) -> Path:
+        if self._config.provisioner:
+            return util.abs_path(
+                Path(self._config.provisioner.directory, "playbooks"),
+            )
+        return Path()
+
+    def _get_playbook(self, section: Section) -> str | None:
         """Return path to playbook or None if playbook is not needed.
 
         Return None when there is no playbook configured and when action is
         considered skippable.
+
+        Args:
+            section: Named section to retrieve playbook for.
+
+        Returns:
+            The playbook path, or none if one is not needed.
         """
         c = self._config.config
-        driver_dict = c["provisioner"]["playbooks"].get(self._config.driver.name)
+        driver_dict: dict[Section, str | None] | None = c["provisioner"]["playbooks"].get(  # type: ignore[assignment]
+            self._config.driver.name,
+        )
 
-        playbook = c["provisioner"]["playbooks"][section]
+        playbook: str | None = c["provisioner"]["playbooks"][section]
         if driver_dict:
             try:
                 playbook = driver_dict[section]
             except Exception as exc:
                 LOG.exception(exc)  # noqa: TRY401
-        if playbook is not None:
+        if self._config.provisioner and playbook is not None:
             playbook = self._config.provisioner.abs_path(playbook)
-            playbook = self._normalize_playbook(playbook)  # type: ignore[no-untyped-call]
+            if playbook:
+                playbook = self._normalize_playbook(playbook)
 
-            if os.path.exists(playbook):  # noqa: PTH110
-                return playbook
-            if os.path.exists(self._get_bundled_driver_playbook(section)):  # type: ignore[no-untyped-call]  # noqa: PTH110
-                return self._get_bundled_driver_playbook(section)  # type: ignore[no-untyped-call]
+                if os.path.exists(playbook):  # noqa: PTH110
+                    return playbook
+
+            if os.path.exists(self._get_bundled_driver_playbook(section)):  # noqa: PTH110
+                return self._get_bundled_driver_playbook(section)
             if section not in [
                 # these playbooks can be considered optional
                 "prepare",
@@ -108,45 +174,49 @@ class AnsiblePlaybooks:
                 return playbook
         return None
 
-    def _get_bundled_driver_playbook(self, section):  # type: ignore[no-untyped-def]  # noqa: ANN001, ANN202
-        path = self._config.driver.get_playbook(section)
+    def _get_bundled_driver_playbook(self, section: Section) -> str:
+        path = self._config.driver.get_playbook(section)  # type: ignore[no-untyped-call]
         if path:
-            return path
+            return path  # type: ignore[no-any-return]
 
-        path = os.path.join(  # noqa: PTH118
-            self._get_playbook_directory(),  # type: ignore[no-untyped-call]
+        path = Path(
+            self._get_playbook_directory(),
             self._config.driver.name,
             self._config.config["provisioner"]["playbooks"][section],
         )
-        if os.path.exists(path):  # noqa: PTH110
-            return path
-        path = os.path.join(  # noqa: PTH118
+        if path.exists():
+            return str(path)
+        path = Path(
             self._config.driver._path,  # noqa: SLF001
             "playbooks",
             self._config.config["provisioner"]["playbooks"][section],
         )
-        return path  # noqa: RET504
+        return str(path)
 
-    def _normalize_playbook(self, playbook):  # type: ignore[no-untyped-def]  # noqa: ANN001, ANN202
+    def _normalize_playbook(self, playbook: str) -> str:
         """Return current filename to use for a playbook by allowing fallbacks.
 
         Currently used to deprecate use of playbook.yml in favour of converge.yml
+
+        Args:
+            playbook: Playbook path to alter.
+
+        Returns:
+            Normalized playbook path.
         """
-        if not playbook or os.path.isfile(playbook):  # noqa: PTH113
+        play_path = Path(playbook)
+        if not playbook or play_path.is_file():
             return playbook
 
         pb_rename_map = {"converge.yml": "playbook.yml"}
-        basename = os.path.basename(playbook)  # noqa: PTH119
+        basename = play_path.name
         if basename in pb_rename_map:
-            fb_playbook = os.path.join(  # noqa: PTH118
-                os.path.dirname(playbook),  # noqa: PTH120
-                pb_rename_map[basename],
-            )
-            if os.path.isfile(fb_playbook):  # noqa: PTH113
+            fb_playbook = play_path.parent / pb_rename_map[basename]
+            if fb_playbook.is_file():
                 LOG.warning(
                     "%s was deprecated, rename it to %s",
-                    pb_rename_map[basename],
+                    fb_playbook.name,
                     basename,
                 )
-                playbook = fb_playbook
+                playbook = str(fb_playbook)
         return playbook
