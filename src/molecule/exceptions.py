09363b52ce94d84f60c52dcea0346f254c5e6de0
Kate Case <kcase@redhat.com>
2025-03-26 12:00:51 -0400

    detail is never used.

--- src/molecule/exceptions.py
+++ src/molecule/exceptions.py
@@ -4,13 +4,10 @@ from __future__ import annotations
 
 import logging
 
-from typing import TYPE_CHECKING, Any
-
-from molecule.util import safe_dump
+from typing import TYPE_CHECKING
 
 
 if TYPE_CHECKING:
-    from collections.abc import Mapping
     from warnings import WarningMessage
 
 
@@ -24,7 +21,6 @@ class ScenarioFailureError(Exception):
         self,
         message: str = "",
         code: int = 1,
-        detail: Mapping[str, Any] | None = None,
         warns: tuple[WarningMessage, ...] = (),
     ) -> None:
         """Custom exception to handle scenario run failures.
@@ -32,16 +28,9 @@ class ScenarioFailureError(Exception):
         Args:
             message: The message to display about the problem.
             code: Exit code to use when exiting.
-            detail: Complex information related to the problem.
             warns: Warnings about the problem to issue.
         """
         super().__init__()
-
-        # detail is usually a multi-line string which is not suitable for normal
-        # logger.
-        if detail:
-            detail_str = safe_dump(detail) if isinstance(detail, dict) else str(detail)
-            print(detail_str)  # noqa: T201
 
         if message:
             LOG.critical(message, extra={"highlighter": False})
