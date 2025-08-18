a8733c0f37f0cb4c3bf7ec5c6a3c6e289f062fe0
Kate Case <kcase@redhat.com>
2025-08-18 13:29:02 -0400

    Fix use of warns in ScenarioFailure

--- src/molecule/exceptions.py
+++ src/molecule/exceptions.py
@@ -4,6 +4,13 @@ from __future__ import annotations
 
 import logging
 
+from typing import TYPE_CHECKING
+
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+    from warnings import WarningMessage
+
 
 LOG = logging.getLogger(__name__)
 
@@ -16,7 +23,7 @@ class MoleculeError(Exception):
         message: str = "",
         code: int = 1,
     ) -> None:
-        """Custom exception to handle scenario run failures.
+        """Generic exception for molecule errors.
 
         Args:
             message: The message to display about the problem.
@@ -29,6 +36,27 @@ class MoleculeError(Exception):
 
 class ScenarioFailureError(MoleculeError):
     """Details about a scenario that failed."""
+
+    def __init__(
+        self,
+        message: str = "",
+        code: int = 1,
+        warns: Sequence[WarningMessage] = (),
+    ) -> None:
+        """Custom exception to handle scenario run failures.
+
+        Args:
+            message: The message to display about the problem.
+            code: Exit code to use when exiting.
+            warns: Warnings about the problem to issue.
+        """
+        super().__init__(message, code)
+
+        if message:
+            LOG.critical(message, extra={"highlighter": False})
+
+        for warn in warns:
+            LOG.warning(warn.message)
 
 
 class ImmediateExit(Exception):  # noqa: N818
