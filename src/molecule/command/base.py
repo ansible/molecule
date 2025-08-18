ec8d355ccd6bb41180bfe76725b28e4983c711ca
Bradley A. Thornton <bthornto@redhat.com>
2025-08-18 12:35:37 -0700

    rm double import

--- src/molecule/command/base.py
+++ src/molecule/command/base.py
@@ -157,7 +157,7 @@ def execute_cmdline_scenarios(
                 glob_str = effective_base_glob.replace("*", scenario_name)
                 configs.extend(get_configs(args, command_args, ansible_args, glob_str))
         except ScenarioFailureError as exc:
-            sysexit_with_message(str(exc), code=exc.code)
+            util.sysexit_with_message(str(exc), code=exc.code)
 
     default_glob = effective_base_glob.replace("*", MOLECULE_DEFAULT_SCENARIO_NAME)
     default_config = None
