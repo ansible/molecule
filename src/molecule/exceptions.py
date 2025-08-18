1e1d0682700a177d88563fad18e37e3de9c0eb5d
Kate Case <kcase@redhat.com>
2025-08-18 12:00:37 -0400

    Restore sysexit_with_message, add sysexit_from_exception

--- src/molecule/exceptions.py
+++ src/molecule/exceptions.py
@@ -4,13 +4,6 @@ from __future__ import annotations
 
 import logging
 
-from typing import TYPE_CHECKING
-
-
-if TYPE_CHECKING:
-    from collections.abc import Sequence
-    from warnings import WarningMessage
-
 
 LOG = logging.getLogger(__name__)
 
@@ -22,24 +15,16 @@ class MoleculeError(Exception):
         self,
         message: str = "",
         code: int = 1,
-        warns: Sequence[WarningMessage] = (),
     ) -> None:
         """Custom exception to handle scenario run failures.
 
         Args:
             message: The message to display about the problem.
             code: Exit code to use when exiting.
-            warns: Warnings about the problem to issue.
         """
         super().__init__()
-
-        if message:
-            LOG.critical(message, extra={"highlighter": False})
-
-        for warn in warns:
-            LOG.warning(warn.__dict__["message"].args[0])
-
         self.code = code
+        self.message = message
 
 
 class ScenarioFailureError(MoleculeError):
